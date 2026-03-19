328a0798b0df92fade53ea27dcc1838cbb603982
nithyashreeprojects <getnrk123@gmail.com>
2026-03-20 00:03:27 +0530

    Switch to gunicorn for production

--- app.py
+++ app.py
@@ -50,5 +50,6 @@ def results():
     matches=get_matches(properties,preferences)
     return render_template('results.html',matches=matches,preferences=preferences)
 
-port = int(os.environ.get("PORT",5001))
-app.run(host="0.0.0.0",port=5001,debug=True)
+if __name__=='__main__':
+    port=int(os.environ.get("PORT",5001))
+    app.run(host="0.0.0.0",port=port,debug=False)
